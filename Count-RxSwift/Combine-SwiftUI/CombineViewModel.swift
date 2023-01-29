589fef2fdc941c5cb39a8a398fa5ae1a6c0a01c5
tosh7 <zlia.6.lj.425@gmail.com>
2023-01-29 14:12:23 +0900

    Fixed library dependencies

--- Count-RxSwift/Combine-SwiftUI/CombineViewModel.swift
+++ Count-RxSwift/Combine-SwiftUI/CombineViewModel.swift
@@ -1,25 +1,10 @@
 import Foundation
-import UIKit
 
 final class CombineViewModel: ObservableObject {
-
-    struct SectionModel: Hashable {
-        var name: String
-        var vc: UIViewController
-
-        func hash(into hasher: inout Hasher) {
-            hasher.combine(name)
-        }
-
-        static func == (lhs: CombineViewModel.SectionModel, rhs: CombineViewModel.SectionModel) -> Bool {
-            lhs.name == rhs.name
-        }
-    }
 
     init() {}
 
     // MARK - Outputs
-    @Published private(set) var items: [SectionModel] = []
 
     // MARK - Inputs
 }
--- Count-RxSwift/Home/HomeViewModel.swift
+++ Count-RxSwift/Home/HomeViewModel.swift
@@ -3,10 +3,25 @@ import UIKit
 import Combine
 
 final class HomeViewModel {
+
+    struct SectionModel: Hashable {
+        var name: String
+        var vc: UIViewController
+
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(name)
+        }
+
+        static func == (lhs: HomeViewModel.SectionModel, rhs: HomeViewModel.SectionModel) -> Bool {
+            lhs.name == rhs.name
+        }
+    }
+
     init() {}
 
     // MARK - OutPuts
     @Published private(set) var nextViewController: UIViewController?
+    @Published private(set) var items: [SectionModel] = []
 
     // MARK - Inputs
     func selectItem(indexPath: IndexPath) {
